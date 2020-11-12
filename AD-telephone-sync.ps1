5d240885aaf54d643ecadb4cd5669397a9c228be
Austin Lane Hearns Howard <austin.lane.howard@gmail.com>
2020-11-11 19:36:36 -0700

    Update AD-telephone-sync.ps1
    Added a file browser to determine which csv is needed

--- AD-telephone-sync.ps1
+++ AD-telephone-sync.ps1
@@ -1,4 +1,11 @@
-$FullList = Import-Csv -Path C:\Users\ahoward\phone-numbers.csv
+#Ask the user for file location
+$FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
+    InitialDirectory = [Environment]::GetFolderPath('Desktop') 
+    Filter = 'Documents (*.csv)|*.csv'
+}
+$null = $FileBrowser.ShowDialog()
+
+$FullList = Import-Csv -Path $FileBrowser.FileName
 $FullList | ForEach-Object{
     $User = Get-ADUser $_.UserID -Properties telephoneNumber,ipPhone
     $User.telephoneNumber = $_.FullPhone
