b162f1c378b932495bc8a843f53db78cb8514e8a
Austin Lane Hearns Howard <austin.lane.howard@gmail.com>
2020-11-11 20:02:12 -0700

    Update AD-telephone-sync.ps1
    Prettifying Code

--- AD-telephone-sync.ps1
+++ AD-telephone-sync.ps1
@@ -1,3 +1,16 @@
+<#
+.SYNOPSIS
+  Updates AD Users Telephone #'s
+.DESCRIPTION
+  Asks for a CSV that includes the headings of FullPhone (users full telephone number), DID (4 digit extension), and UserID (UPN) then automatically goes through and updates their AD accounts with the relevant information.  
+.NOTES
+  Version:        1.0
+  Author:         Captain Howard
+  Creation Date:  11/11/20
+  Purpose/Change: Production 
+
+#>
+
 #Ask the user for file location
 $FileBrowser = New-Object System.Windows.Forms.OpenFileDialog -Property @{ 
     InitialDirectory = [Environment]::GetFolderPath('Desktop') 
